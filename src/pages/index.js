517b5d5fa6ea5664dc417d82c6ba4f3f3a698b28
oluwole fadeyi <16049411+tfadeyi@users.noreply.github.com>
2023-10-08 12:45:32 +0100

    Fix layout
    Signed-off-by: oluwole fadeyi <16049411+tfadeyi@users.noreply.github.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,46 +14,48 @@ function HomepageHeader() {
 
   return (
     <header className={clsx('hero', styles.heroBanner)}>
-      <div className="container"  style={{ display: "flex"}}>
-        <div className="container">
-            <h1 className="hero__title">{siteConfig.tagline}</h1>
-            <p className="hero__body">
-                error.fyi is an open-source project dedicated to improving the narrative regarding user-facing errors in software applications, with the ultimate aim of enhancing the end-user experience.
-                It accomplishes this by providing developers with tools and libraries that facilitate the creation, documentation, and management of errors within applications.
-            </p>
-            <div>
-                <Link
-                    className={clsx("button button--primary button--lg", styles.buttons)}
-                    to="/docs/intro">
-                    Get Started
-                </Link>
-                <Link
-                    className={clsx("button  button--secondary button--lg", styles.buttons)}
-                    to="/docs/category/installation">
-                    Installation
-                </Link>
-            </div>
-        </div>
-        <div className="container">
-            <Splide options={{autoplay: true}} aria-label="Examples">
-                <SplideSlide>
-                    <img src="/img/landing/code.png" alt="code_example"/>
-                </SplideSlide>
-                <SplideSlide>
-                    <img src="/img/landing/manifest.png" alt="manifest"/>
-                </SplideSlide>
-                <SplideSlide>
-                    <img src="/img/landing/error_cli.png" alt="error_example"/>
-                </SplideSlide>
-                <SplideSlide>
-                    <img src="/img/landing/markdown.png" alt="generated_markdown"/>
-                </SplideSlide>
-                <SplideSlide>
-                    <img src="/img/landing/static_site.png" alt="static_site"/>
-                </SplideSlide>
+      <div className="container">
+            <div className="container row">
+                <div className="container col col--6">
+                    <h1 className="hero__title">{siteConfig.tagline}</h1>
+                    <p className="hero__body">
+                        error.fyi is an open-source project with the aim of improving user-facing errors in applications, enhancing the end-user experience.
+                        It accomplishes this by providing developers with tools and libraries to enrich applications' errors with <strong>additional error context</strong>, including <strong>causes, fixes, and documentation</strong> whenever errors are returned to their users.
+                    </p>
+                    <div>
+                        <Link
+                            className={clsx("button button--primary button--lg", styles.buttons)}
+                            to="/docs/intro">
+                            Get Started
+                        </Link>
+                        <Link
+                            className={clsx("button  button--secondary button--lg", styles.buttons)}
+                            to="/docs/category/installation">
+                            Installation
+                        </Link>
+                    </div>
+                </div>
+                <div className="container col col--6">
+                    <Splide options={{autoplay: true}} aria-label="Examples">
+                        <SplideSlide>
+                            <img src="/img/landing/code.png" alt="code_example"/>
+                        </SplideSlide>
+                        <SplideSlide>
+                            <img src="/img/landing/manifest.png" alt="manifest"/>
+                        </SplideSlide>
+                        <SplideSlide>
+                            <img src="/img/landing/error_cli.png" alt="error_example"/>
+                        </SplideSlide>
+                        <SplideSlide>
+                            <img src="/img/landing/markdown.png" alt="generated_markdown"/>
+                        </SplideSlide>
+                        <SplideSlide>
+                            <img src="/img/landing/static_site.png" alt="static_site"/>
+                        </SplideSlide>
 
-            </Splide>
-        </div>
+                    </Splide>
+                </div>
+            </div>
       </div>
     </header>
   );
